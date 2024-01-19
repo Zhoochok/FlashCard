4fcff70c04f80d3b5a0056faded8b0e8b5dc42a1
alyonaosipova <alyonaosipova@gmail.com>
2024-01-19 15:58:01 +0300

    addController

--- quiz-controller.js
+++ quiz-controller.js
@@ -1,26 +1,24 @@
-const inquirer = require('inquirer');
-const fs = require('fs').promises;
-const { theme1, theme2 } = require('./quiz-model');
+const inquirer = require("inquirer");
+const fs = require("fs").promises;
+const { theme1, theme2 } = require("./quiz-model");
 
 async function start() {
   await inquirer
     .prompt([
       {
-        type: 'list',
-        name: 'theme',
-        message: 'Выберите тему:',
-        choices: ['Memes', 'Song'],
+        type: "list",
+        name: "theme",
+        message: "Выберите тему:",
+        choices: ["Memes", "Song"],
       },
     ])
     .then((themes) => {
-      if (themes.theme === 'Memes') {
+      if (themes.theme === "Memes") {
         theme1();
       }
-      if (themes.theme === 'Song') {
+      if (themes.theme === "Song") {
         theme2();
       }
     });
 }
 start();
-// console.clear()
-// start()
--- quiz-model.js
+++ quiz-model.js
@@ -1,25 +1,32 @@
 /* eslint-disable no-await-in-loop */
 /* eslint-disable linebreak-style */
-const fs = require('fs').promises;
-const inquirer = require('inquirer');
-const { EOL } = require('os');
+const fs = require("fs").promises;
+const inquirer = require("inquirer");
 
 async function theme1() {
-  const tema = await fs.readFile('./questions1.json', 'utf8');
+  const tema = await fs.readFile("./questions1.json", "utf8");
   const parsedTema = JSON.parse(tema);
 
   let count = 0;
   for (let i = 0; i < parsedTema.length; i += 1) {
     const vopros = await inquirer.prompt([
-
       {
-        type: 'list',
-        name: 'point',
+        type: "list",
+        name: "point",
         message: `Vopros: ${parsedTema[i].question}`,
         choices: [
-          { name: `${parsedTema[i].answers[0]}`, value: parsedTema[i].points[0] },
-          { name: `${parsedTema[i].answers[1]}`, value: parsedTema[i].points[1] },
-          { name: `${parsedTema[i].answers[2]}`, value: parsedTema[i].points[2] },
+          {
+            name: `${parsedTema[i].answers[0]}`,
+            value: parsedTema[i].points[0],
+          },
+          {
+            name: `${parsedTema[i].answers[1]}`,
+            value: parsedTema[i].points[1],
+          },
+          {
+            name: `${parsedTema[i].answers[2]}`,
+            value: parsedTema[i].points[2],
+          },
         ],
       },
     ]);
@@ -29,24 +36,31 @@ async function theme1() {
     console.log(`Right now u have: ${count} points`);
   }
 }
-// theme1();
 
 async function theme2() {
-  const tema = await fs.readFile('./questions2.json', 'utf8');
+  const tema = await fs.readFile("./questions2.json", "utf8");
   const parsedTema = JSON.parse(tema);
 
   let count = 0;
   for (let i = 0; i < parsedTema.length; i += 1) {
     const vopros = await inquirer.prompt([
-
       {
-        type: 'list',
-        name: 'point',
+        type: "list",
+        name: "point",
         message: `Vopros: ${parsedTema[i].question}`,
         choices: [
-          { name: `${parsedTema[i].answers[0]}`, value: parsedTema[i].points[0] },
-          { name: `${parsedTema[i].answers[1]}`, value: parsedTema[i].points[1] },
-          { name: `${parsedTema[i].answers[2]}`, value: parsedTema[i].points[2] },
+          {
+            name: `${parsedTema[i].answers[0]}`,
+            value: parsedTema[i].points[0],
+          },
+          {
+            name: `${parsedTema[i].answers[1]}`,
+            value: parsedTema[i].points[1],
+          },
+          {
+            name: `${parsedTema[i].answers[2]}`,
+            value: parsedTema[i].points[2],
+          },
         ],
       },
     ]);
@@ -55,32 +69,5 @@ async function theme2() {
     console.log(`Right now u have: ${count} points`);
   }
 }
-// theme2();
 
-// async function theme3() {
-//   const tema = await fs.readFile('./questions3.json', 'utf8');
-//   const parsedTema = JSON.parse(tema);
-
-//   let count = 0;
-//   for (let i = 0; i < parsedTema.length; i += 1) {
-//     const vopros = await inquirer.prompt([
-
-//       {
-//         type: 'list',
-//         name: 'point',
-//         message: `Vopros: ${parsedTema[i].question}`,
-//         choices: [
-//           { name: `${parsedTema[i].answers[0]}`, value: parsedTema[i].points[0] },
-//           { name: `${parsedTema[i].answers[1]}`, value: parsedTema[i].points[1] },
-//           { name: `${parsedTema[i].answers[2]}`, value: parsedTema[i].points[2] },
-//         ],
-//       },
-//     ]);
-//     count += vopros.point;
-//     console.log(count);
-//     console.log(`Right now u have: ${count} points`);
-//   }
-// }
-// theme3();
-
-module.exports = { theme1, theme2, };
+module.exports = { theme1, theme2 };
